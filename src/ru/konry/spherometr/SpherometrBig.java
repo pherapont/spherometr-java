67f2705b0d97b54e68766f77fa617ee52c56a3e4
pherapont <pherapont@yandex.ru>
2021-04-12 06:28:43 +0700

    class SpherometrBig TODO: надо вынести данные, относящиеся к вычислению из конструктора в метод calculate

--- src/ru/konry/spherometr/SpherometrBig.java
+++ src/ru/konry/spherometr/SpherometrBig.java
@@ -4,21 +4,18 @@ import ru.konry.spherometr.parametrs.SurfaceClearance;
 import ru.konry.spherometr.parametrs.SurfaceRadius;
 import ru.konry.spherometr.parametrs.SurfaceType;
 
-public class SpherometrSmall
+public class SpherometrBig
 {
-    public enum SpherSmallData {
+    public enum SpherBigData {
 
-        RING_1(59.9797, 4.725),
-        RING_2(42.504, 5.15),
-        RING_3(29.964, 4.345),
-        RING_4(21.2656, 3.153),
-        RING_5(15.0073, 2.375),
-        RING_7(7.477, 1.17);
+        RING_1(45.0473, 3.158),
+        RING_2(74.9673, 3.159),
+        RING_3(109.9832, 3.157),
+        RING_4(149.982, 3.160);
 
         public final double ringRadius;
         public final double ballRadius;
-
-        SpherSmallData(double ringRadius, double ballRadius) {
+        SpherBigData(double ringRadius, double ballRadius) {
             this.ringRadius = ringRadius;
             this.ballRadius = ballRadius;
         }
@@ -30,8 +27,8 @@ public class SpherometrSmall
     ClearanceCalculator clearanceCalculator;
     SurfaceType type;
 
-    public SpherometrSmall(int ringNumber, SurfaceClearance measure, SurfaceType type) {
-        SpherSmallData sphereType = SpherSmallData.values()[ringNumber - 1];
+    public SpherometrBig(int ringNumber, SurfaceClearance measure, SurfaceType type) {
+        SpherBigData sphereType = SpherBigData.values()[ringNumber - 1];
         ringRadius = sphereType.ringRadius;
         ballRadius = sphereType.ballRadius;
         this.type = type;
@@ -40,15 +37,14 @@ public class SpherometrSmall
         );
     }
 
-    public SpherometrSmall(int ringNumber, SurfaceRadius measure, SurfaceType type) {
-        SpherSmallData sphereType = SpherSmallData.values()[ringNumber - 1];
+    public SpherometrBig(int ringNumber, SurfaceRadius measure, SurfaceType type) {
+        SpherBigData sphereType = SpherBigData.values()[ringNumber - 1];
         ringRadius = sphereType.ringRadius;
         ballRadius = sphereType.ballRadius;
         clearanceCalculator = new ClearanceCalculator(
                 measure.radius, ringRadius, ballRadius
         );
     }
-
     public double calculate() {
         double result;
         if(type == SurfaceType.CONCAVE) {
